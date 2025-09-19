c297740f2157e5a4f9d3e8ab5c3a1c724c5ac040
Lena Le <ll224ve@student.lnu.se>
2025-09-19 23:50:49 +0200

    feat(parser): add format validation for token sequence

--- src/Parser.js
+++ src/Parser.js
@@ -11,27 +11,47 @@ export class Parser {
     let validTokens = []
 
     // bad readability
-    for (let i = 0; i < tokens.length; i++) {
+    for (let i = 0; i < tokens.length - 1; i++) {
       let numberedToken = Number(tokens[i])
       if (!Number.isNaN(numberedToken)) {
         validTokens.push(tokens[i])
       } else if (this.validOperators.includes(tokens[i])) {
         validTokens.push(tokens[i])
       } else {
-        throw new Error("Invalid expression input. Invalid operator or includes NaN!")
+        throw new Error("Invalid expression input. Expression includes invalid operator or NaN!")
       }
     }
 
     let validExpression = [] // valid tokens and format
 
-    // validate format: invalid if two numbers or operators come in a row
-    // 1. must start and end with a number
-    // 2. if current token is a number the previous has to be an operator and vice versa
+    // check first token
+    if (this.isOperator(validTokens[0])) {
+      throw new Error("Expression cannot start with an operator")
+    }
+    // check last token
+    if (this.isOperator(validTokens[validTokens.length - 1])) {
+      throw new Error("Expression cannot end with an operator")
+    }
+
+    // check sequence of tokens, starting at index 1
+    for (let i = 1; i < validTokens.length; i++) {
+      let previous = validTokens[i-1]
+      let current = validTokens[i]
+
+      if (this.isNumber(current) && this.isNumber(previous)) {
+        throw new Error("Two numbers in a row is not allowed")
+      }
+
+      if (this.isOperator(current) && this.isOperator(previous)) {
+        throw new Error("Two operators in a row is not allowed")
+      }
+    }
   }
 
   /**
    * Checks if a token is an operator
-   * 
+   * @param the token to be checked
+   * @returns boolean
   */  
   isOperator(token) {
     return this.validOperators.includes(token)
@@ -39,8 +59,8 @@ export class Parser {
 
   /**
    * Checks if a token is a number
-   * @param 
-   * @returns 
+   * @param the token to be checked
+   * @returns boolean
    */
   isNumber(token) {
     const convert = Number(token)
