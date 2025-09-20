ee8b59807e96bcf8e58448b0025ce99c923c2c49
Lena Le <ll224ve@student.lnu.se>
2025-09-20 15:41:01 +0200

    refactor(parser): extract format validation into smaller helper methods for readability

--- src/Parser.js
+++ src/Parser.js
@@ -23,29 +23,6 @@ export class Parser {
     }
 
     let validExpression = [] // valid tokens and format
-
-    // check first token
-    if (this.isOperator(validTokens[0])) {
-      throw new Error("Expression cannot start with an operator")
-    }
-    // check last token
-    if (this.isOperator(validTokens[validTokens.length - 1])) {
-      throw new Error("Expression cannot end with an operator")
-    }
-
-    // check sequence of tokens, starting at index 1
-    for (let i = 1; i < validTokens.length; i++) {
-      let previous = validTokens[i-1]
-      let current = validTokens[i]
-
-      if (this.isNumber(current) && this.isNumber(previous)) {
-        throw new Error("Two numbers in a row is not allowed")
-      }
-
-      if (this.isOperator(current) && this.isOperator(previous)) {
-        throw new Error("Two operators in a row is not allowed")
-      }
-    }
   }
 
   /**
@@ -69,4 +46,40 @@ export class Parser {
     } else return true
   }
 
+
+  /**
+   * Checks the first token of an expression
+   */
+  checkFirstToken() { // good understandability
+    if (this.isOperator(validTokens[0])) {
+      throw new Error("Expression cannot start with an operator")
+    }
+  }
+
+  /**
+   * Check the last token of an expression
+   */
+  checkLastToken() {
+    if (this.isOperator(validTokens[validTokens.length - 1])) {
+      throw new Error("Expression cannot end with an operator")
+    }
+  }
+
+  /**
+   * Check the sequence of the expression to not have two operators or numbers in a row
+   */
+  checkSequence() {
+    for (let i = 1; i < validTokens.length; i++) {
+      let previous = validTokens[i-1]
+      let current = validTokens[i]
+
+      if (this.isNumber(current) && this.isNumber(previous)) {
+        throw new Error("Two numbers in a row is not allowed")
+      }
+
+      if (this.isOperator(current) && this.isOperator(previous)) {
+        throw new Error("Two operators in a row is not allowed")
+      }
+    }
+  }
 }
