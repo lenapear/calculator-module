ad4e345fff855a255d1ce212dd7c1107d1f70846
Lena Le <ll224ve@student.lnu.se>
2025-09-20 18:03:36 +0200

    feat(parser): complete tokenization and add expression validation using helpers

--- src/Parser.js
+++ src/Parser.js
@@ -4,25 +4,45 @@ export class Parser {
 
   constructor(expression) {
     this.expression = expression
+    this.tokenize(expression)
   }
 
-  #tokenize(expression) {
-    const tokens = expression.split(" ") // force space format for now
-    let validTokens = []
+  tokenize(expression) {
+    const tokenizedExpression = this.splitTokens(expression)
+    this.validTokens = this.validateTokens(tokenizedExpression)
 
+    this.checkFirstToken()
+    this.checkLastToken()
+    this.checkSequence()
+
+    return this.validTokens
+  }
+
+  /**
+   * Splits the expression into tokens
+   * @param {*} expression 
+   */
+  splitTokens(expression) {
+    return expression.split(" ") // force space format for now
+  }
+
+  /**
+   * Validate tokens
+   * @param the tokens to be validated
+   */
+  validateTokens(tokens) {
     for (let i = 0; i < tokens.length; i++) {
       let currentToken = tokens[i]
 
       if (this.isNumber(currentToken)) {
-        validTokens.push(Number(currentToken)) // convert it into a number first
+        this.validTokens.push(Number(currentToken)) // convert it into a number first
       } else if (this.isOperator(currentToken)) {
-        validTokens.push(currentToken)
+        this.validTokens.push(currentToken)
       } else {
         throw new Error("Invalid expression input. Expression includes invalid operator or NaN!")
       }
     }
-
-    let validExpression = [] // valid tokens and format
+    return this.validTokens
   }
 
   /**
@@ -51,7 +71,7 @@ export class Parser {
    * Checks the first token of an expression
    */
   checkFirstToken() { // good understandability
-    if (this.isOperator(validTokens[0])) {
+    if (this.isOperator(this.validTokens[0])) {
       throw new Error("Expression cannot start with an operator")
     }
   }
@@ -60,7 +80,7 @@ export class Parser {
    * Check the last token of an expression
    */
   checkLastToken() {
-    if (this.isOperator(validTokens[validTokens.length - 1])) {
+    if (this.isOperator(this.validTokens[this.validTokens.length - 1])) {
       throw new Error("Expression cannot end with an operator")
     }
   }
@@ -69,9 +89,9 @@ export class Parser {
    * Check the sequence of the expression to not have two operators or numbers in a row
    */
   checkSequence() {
-    for (let i = 1; i < validTokens.length; i++) {
-      let previous = validTokens[i-1]
-      let current = validTokens[i]
+    for (let i = 1; i < this.validTokens.length; i++) {
+      let previous = this.validTokens[i-1]
+      let current = this.validTokens[i]
 
       if (this.isNumber(current) && this.isNumber(previous)) {
         throw new Error("Two numbers in a row is not allowed")
