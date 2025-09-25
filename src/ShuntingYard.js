c3b800b603f28ba20383464a1c934c32e6e7e203
Lena Le <ll224ve@student.lnu.se>
2025-09-25 23:43:51 +0200

    refactor(shunting-yard): remove constructor and make class stateless

--- src/ShuntingYard.js
+++ src/ShuntingYard.js
@@ -6,10 +6,13 @@
 import { isOperator, isNumber } from "./Parser.js"
 
 export class ShuntingYard {
-  constructor(infixTokens) {
-    this.postfixTokens = this.rpnConverter(infixTokens)
-  }
 
+  /**
+   * Converts the infixTokens to postfix in the RPN format.
+   * 
+   * @param {array} infixTokens 
+   * @returns {array} outputQueue
+   */
   toPostfix(infixTokens) {
     let operatorStack = []
     let outputQueue = []
@@ -18,32 +21,57 @@ export class ShuntingYard {
       let current = infixTokens[i]
 
       if (isNumber(current)) {
-        outputQueue.push(current)
+        this.handleNumber(current, outputQueue)
       } else if (isOperator(current)) {
-          let prevOperator = operatorStack[operatorStack.length -1]
-
-          while (operatorStack.length > 0 && this.hasPrecedence(prevOperator, current)){
-            outputQueue.push(operatorStack.pop()) 
-            prevOperator = operatorStack[operatorStack.length - 1]
+          while (operatorStack.length > 0 && this.hasPrecedence(operatorStack[operatorStack.length -1], current)){
+            this.handleOperator(outputQueue, operatorStack)
           }
           operatorStack.push(current)
         }
-    } // for-loop bracket
+    }
 
-    // finished RPN
+    this.flushStack(operatorStack, outputQueue)
+    return outputQueue
+  }
+
+  /**
+   * Handles a number token by adding it to the output queue.
+   *
+   * @param {number} number - The number token.
+   * @param {Array} outputQueue - The output queue being built.
+   */
+  handleNumber(number, outputQueue) {
+    outputQueue.push(number)
+  }
+
+  /**
+   * Handles the top operator in the operatorStack by adding it to outputQueue.
+   * @param {array} outputQueue 
+   * @param {array} operatorStack 
+   */
+  handleOperator(outputQueue, operatorStack) {
+    outputQueue.push(operatorStack.pop())
+  }
+
+  /**
+   * Empties all remaining operators from the stack into the output queue.
+   *
+   * @param {Array} operatorStack
+   * @param {Array} outputQueue
+   */
+  flushStack(operatorStack, outputQueue) {
     while (operatorStack.length > 0) {
       let currentOp = operatorStack.pop()
       outputQueue.push(currentOp)
     }
-    return outputQueue
   }
 
   /**
    * Compares the precedence of two operators.
    *
-   * @param {string} op1 - the operator on top of the operatorStack.
-   * @param {string} op2 - the current operator being evaluated.
-   * @returns boolean
+   * @param {string} op1 - The operator on top of the operatorStack.
+   * @param {string} op2 - The current operator being evaluated.
+   * @returns {boolean}
    */
   hasPrecedence(op1, op2) {
     return this.getPrecedence(op1) >= this.getPrecedence(op2)
@@ -52,8 +80,9 @@ export class ShuntingYard {
   /**
    * Checks if an operator has lower or higher precedence according to PEMDAS.
    *
-   * @param {*} operator 
-   * @returns 
+   * @param {string} operator - The operator
+   * @returns {number} - Precedence level (2 = high, 1 = low).
+   * @throws {error} If the operator is unknown
    */
   getPrecedence(operator) {
     if (["*", "/"].includes(operator)) {
@@ -62,5 +91,4 @@ export class ShuntingYard {
       return 1
     } else throw new Error(`Unknown operator: ${operator}`)
   }
-
-} // class bracket
+}
