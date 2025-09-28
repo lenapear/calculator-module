e7d8b4c108324822262a8a3b934eabd309ef9795
Lena Le <ll224ve@student.lnu.se>
2025-09-28 12:52:19 +0200

    feat(calculator): implement the initial structure of Calculator class

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,11 +3,21 @@
  * lena's notes: orchestrates the flow
  */
 
+import { Evaluator } from "./Evaluator.js"
 import { Parser } from "./Parser.js"
+import { ShuntingYard} from "./ShuntingYard.js"
+
 
 export class Calculator {
   constructor(expression) {
-    this.expression = expression // can this have the same name as in Parser.js?? double check
+    this.expression = expression
+    this.calculate(expression)
+  }
+
+  calculate(expression) {
+    const validatedExpression = this.validateExpression(expression)
+    const convertedExpression = this.convertExpression(validatedExpression)
+    return this.evaluateExpression(convertedExpression)
   }
 
   /**
@@ -15,8 +25,21 @@ export class Calculator {
    * @param {*} expression 
    */
   validateExpression(expression) {
-    const Parser = new Parser()
-    Parser.tokenize(expression)
+    const parser = new Parser()
+    const infixExpression = parser.tokenize(expression)
+    return infixExpression
+  }
+
+  convertExpression(infixExpression) {
+    const shuntingYard = new ShuntingYard()
+    const postfixExpression = shuntingYard.toPostfix(infixExpression)
+    return postfixExpression
+  }
+
+  evaluateExpression(postfixExpression) {
+    const evaluator = new Evaluator()
+    const evaluatedExpression = evaluator.evaluate(postfixExpression)
+    return evaluatedExpression
   }
 }
 
