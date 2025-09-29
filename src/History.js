c74c18cc064771f5467aa0b5f57494e6839f35b6
Lena Le <ll224ve@student.lnu.se>
2025-09-29 15:49:39 +0200

    fix(add)

--- src/History.js
+++ src/History.js
@@ -8,7 +8,7 @@ export class History {
   }
 
   add(expression, result) {
-    this.calculationHistory.expression = result
+    this.calculationHistory[expression] = result
   }
 
   get(expression) {
